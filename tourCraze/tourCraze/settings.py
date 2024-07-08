8dde04fb66eaeb68f05895449221a9ec145d1408
Jassi-Gill <jaswindar.gill22b@iiitg.ac.in>
2024-07-08 11:52:36 +0530

    managedSettings

--- tourCraze/tourCraze/settings.py
+++ tourCraze/tourCraze/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "base.apps.BaseConfig",
 ]
+
+# AUTH_USER_MODEL = "base.User"
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'tourCraze.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = "/images/" 
+MEDIA_ROOT = BASE_DIR / "static/images" 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
