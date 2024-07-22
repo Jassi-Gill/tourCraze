d0d44c00732b6e4bda0ad26920e82a1d3e2384fc
Jassi-Gill <jaswindar.gill22b@iiitg.ac.in>
2024-07-22 14:24:25 +0530

    c1

--- tourCraze/base/models.py
+++ tourCraze/base/models.py
@@ -1,17 +1,17 @@
 from django.db import models
-# from django.contrib.auth.models import AbstractUser
-# from django.core.validators import MinValueValidator, MaxValueValidator
-# # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+# Create your models here.
 
-# class User(AbstractUser):
-#     name = models.CharField(max_length=200, null=True)
-#     email = models.EmailField(unique=True, null=True)
-#     bio = models.TextField(null=True)
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=True)
+    bio = models.TextField(null=True)
 
-#     avatar = models.ImageField(null=True, default="avatar.svg")
+    avatar = models.ImageField(null=True, default="avatar.svg")
 
-#     USERNAME_FIELD = "email"
-#     REQUIRED_FIELDS = []
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
 # class Location(models.Model):
 #     name = models.CharField(max_length=200, null=True)
@@ -23,3 +23,6 @@ from django.db import models
 #         default = 0,
 #     )
 #     emgCon = models.ForeignKey("EmgCon", on_delete= models.SET_NULL, null = True)
+    
+# class EmgCon(models.Model):
+#     num =  models.TextField(null = True)
--- tourCraze/base/urls.py
+++ tourCraze/base/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     # path("login/", views.loginPage, name="login"),
     # path("logout/", views.logoutUser, name="logout"),
     # path("register/", views.registerPage, name="register"),
-    # path("", views.home, name="home"),
+    path("", views.login , name="login"),
 ]
