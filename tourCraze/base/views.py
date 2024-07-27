0181d37794da3d09aca99a1ba8d900ab8962fc4e
Jassi-Gill <jaswindar.gill22b@iiitg.ac.in>
2024-07-27 17:59:35 +0530

    c1

--- tourCraze/base/views.py
+++ tourCraze/base/views.py
@@ -8,13 +8,35 @@ from .forms import MyUserCreationForm
 from django.http import HttpResponse
 
 def login_page(request):
+    if request.method == "POST":
+        email = request.POST.get("email").lower()
+        password = request.POST.get("password")
+
+        try:
+            user = User.objects.get(email=email)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username or Password does not match!")
+
     return render(request, "base/login_page.html")
 
 def register_page(request):
     form = MyUserCreationForm()
 
     if request.method == "POST":
-        form = MyUserCreationForm(request.POST)
+        form.username = request.POST.get("username")
+        form.name = request.POST.get("name")
+        form.email = request.POST.get("email")
+        form.password1 = request.POST.get("password1")
+        form.password2 = request.POST.get("password2")
+
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
