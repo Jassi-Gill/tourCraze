8de6a4e9a7b207c48c864a948c28e85d7a05a73a
Jassi-Gill <jaswindar.gill22b@iiitg.ac.in>
2024-07-27 19:02:58 +0530

    j1

--- tourCraze/base/urls.py
+++ tourCraze/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path("register/", views.register_page, name="register_page"),
     path("", views.login_page , name="login_page"),
+    path("home", views.home_page , name="home_page"),
 ]
--- tourCraze/base/views.py
+++ tourCraze/base/views.py
@@ -7,6 +7,7 @@ from .models import CusUser
 from .forms import MyUserCreationForm
 from django.http import HttpResponse
 
+
 def login_page(request):
     if request.method == "POST":
         username = request.POST.get("username").lower()
@@ -16,29 +17,38 @@ def login_page(request):
 
         if user is not None:
             login(request, user)
-            return redirect("register_page")
+            return redirect("home_page")
         else:
             messages.error(request, "Username or Password does not match!")
 
     return render(request, "base/login_page.html")
 
+
 def register_page(request):
     form = MyUserCreationForm()
 
     if request.method == "POST":
+        form = MyUserCreationForm(request.POST)
         form.username = request.POST.get("username")
         form.name = request.POST.get("name")
         form.email = request.POST.get("email")
         form.password1 = request.POST.get("password1")
         form.password2 = request.POST.get("password2")
 
+        print(form.username, form.name, form.email, form.password1, form.password2)
+
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
             user.save()
             login(request, user)
-            return redirect("login-page")
+            return redirect("home_page")
         else:
             messages.error(request, "An Error occured during registration!")
     context = {"form": form}
     return render(request, "base/register_page.html", context)
+
+
+def home_page(request):
+    context = {}
+    return render(request, "base/home_page.html", context)
